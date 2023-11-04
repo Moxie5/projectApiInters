0539d3f6d4211861c289e1d2f8504fdf4f5f325e
Moxie5 <dobromir00@gmail.com>
2023-11-04 18:52:37 +0200

    fix password

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -25,7 +25,7 @@ class UsersTableSeeder extends Seeder
                 'name' => 'admin',
                 'email' => 'admin@abv.bg',
                 'email_verified_at' => NULL,
-                'password' => '$2y$10$0RluYod2oYPr7La4x35vUu1Y/OlN9iOhQKMkCAs4Wix.hbq31KRua',
+                'password' => '$2a$12$5zkbvPfepWJlbTJkQUu0q.KB2YrcpUxvozUAMxLWillE4nIlF6bZC',
                 'remember_token' => NULL,
                 'created_at' => '2023-10-27 07:10:45',
                 'updated_at' => '2023-11-01 17:20:26',
